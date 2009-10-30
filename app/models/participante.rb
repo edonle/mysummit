bd49621e7b4873a40785ea8f8f077472533d24bb
Edson <edonle@gmail.com>
2009-10-30 17:53:09 -0200

    Engatinhando com RSpec

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -1,3 +1,5 @@
 class Participante < ActiveRecord::Base
+  validates_presence_of :nome, :message => "não pode ser em branco"
+  validates_length_of :nome, :within => 2..50, :too_long => "muito longo", :too_short => "muito curto"
 end
 
--- spec/models/participante_spec.rb
+++ spec/models/participante_spec.rb
@@ -1,20 +1,53 @@
 require 'spec_helper'
 
+module ParticipanteSpecHelper
+  def valid_participante_attributes
+    {
+      :nome => 'Edson'
+    }
+  end
+end
+
 describe Participante do
 
-  it "should have a nome" do
+  include ParticipanteSpecHelper
 
-    participante = Participante.new
-    participante.nome = "Edson"
-    participante.nome.should be == "Edson"
+  before do
+
+    @participante = Participante.new
   end
 
-  it "should be valid" do
+#  Comentários
 
-    participante = Participante.new
-    participante.nome = "Edson"
-    participante.should be_valid
+  it "deve ser valido" do
+
+    @participante.attributes = valid_participante_attributes
+    @participante.should be_valid
   end
 
+  it "nome é requerido" do
+    @participante.attributes = valid_participante_attributes.except(:nome)
+    @participante.nome = ''
+    @participante.should_not be_valid
+    @participante.errors.on(:nome).should include("não pode ser em branco")
+    @participante.nome = "Edson"
+    @participante.should be_valid
+  end
+
+  it "deve ter no maximo 50 caracteres" do
+
+    @participante.attributes = valid_participante_attributes.except(:nome)
+    @participante.nome = 'Edson Edson Edson Edson Edson Edson Edson Edson Edson Edson '
+    @participante.should_not be_valid
+    @participante.errors.on(:nome).should include("muito longo")
+  end
+
+  it "deve ter no minimo 2 caracteres" do
+
+    @participante.attributes = valid_participante_attributes.except(:nome)
+    @participante.nome = 'E'
+    @participante.should_not be_valid
+    @participante.errors.on(:nome).should include("muito curto")
+  end
 end
 
